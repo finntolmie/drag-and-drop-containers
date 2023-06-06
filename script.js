2c42e9e8a6dff242914c619c6946d5346c78fd66
finntolmie <50947941+finntolmie@users.noreply.github.com>
2023-06-06 20:47:33 +1000

    placeholders and resizeable containers

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.querySelectorAll(".bounds").forEach((item) => {
 });
 
 document.querySelectorAll(".draggable").forEach((item) => {
+	let placeholder = document.createElement("div");
+	placeholder.classList.add("placeholder");
 	item.ondragstart = function () {
 		return false;
 	};
@@ -19,15 +21,43 @@ document.querySelectorAll(".draggable").forEach((item) => {
 
 		function onDrag(e) {
 			moveTo(e.pageX, e.pageY);
+			let elementBelow = document.elementFromPoint(e.clientX, e.clientY);
+
+			if (!elementBelow.classList.contains("placeholder")) {
+				if (elementBelow.classList.contains("draggable")) {
+					if (
+						elementBelow.getBoundingClientRect().left >
+						item.getBoundingClientRect().left
+					) {
+						elementBelow.insertAdjacentElement("beforebegin", placeholder);
+					} else {
+						elementBelow.insertAdjacentElement("afterend", placeholder);
+					}
+				} else if (elementBelow.classList.contains("bounds")) {
+					elementBelow.appendChild(placeholder);
+				}
+			}
 		}
 
 		function onLetGo(e) {
 			let elementBelow = document.elementFromPoint(e.clientX, e.clientY);
-			if (elementBelow.classList.contains("draggable")) {
+			if (elementBelow.classList.contains("placeholder")) {
 				elementBelow.insertAdjacentElement("beforebegin", item);
+				placeholder.remove();
+			}
+			if (elementBelow.classList.contains("draggable")) {
+				if (
+					elementBelow.getBoundingClientRect().left >
+					item.getBoundingClientRect().left
+				) {
+					elementBelow.insertAdjacentElement("beforebegin", item);
+				} else {
+					elementBelow.insertAdjacentElement("afterend", item);
+				}
 			} else if (elementBelow.classList.contains("bounds")) {
 				elementBelow.appendChild(item);
 			}
+			placeholder.remove();
 			item.classList.remove("dragging");
 			document.removeEventListener("mousemove", onDrag);
 			document.removeEventListener("mouseup", onLetGo);
